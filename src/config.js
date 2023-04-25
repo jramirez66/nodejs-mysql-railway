7d0ce031df132ae24a0436eaefca479285a5cede
jramirez66 <juanoramirez619@gmail.com>
2023-04-24 20:40:19 -0500

    First commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 3000;
+
+export const DB_HOST = process.env.DB_HOST || '127.0.0.1';
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '';
+export const DB_NAME = process.env.DB_NAME || 'usersdb';
+export const DB_PORT = process.env.DB_PORT || 3306;
